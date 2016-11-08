5feab6a9b6dfc3e13db1b90c7d76d5f20ecbe071
Jason Lenoble <jason.lenoble@gmail.com>
2016-11-09 00:40:52 +0100

    Refactored keyFunc
    Much cleaner structure and generated key func now allows for any number
    of args.

--- src/keyfunc.js
+++ src/keyfunc.js
@@ -1,127 +1,35 @@
-import signature from 'sig';
-
-function strictKeyFunc(options = {}) {
-  options = Object.assign({stem: 'key'}, options);
-  var key;
-
-  if (options.type) {
-    let type = options.type;
-    if (type === String) {
-      type = 'string';
-    } else if (type === Number) {
-      type = 'number';
-    } else if (type === Object) {
-      type = 'object';
-    } else if (type === Array) {
-      type = 'array';
-    }
-
-    if (type === 'string' || type === 'number') {
-      key = (function(stem, type) {
-        return inst => {
-          if (typeof inst !== type) {
-            throw new TypeError(
-              `Expected type ${type} or Number for ${JSON.stringify(inst)}`);
-          }
-          return stem + inst;
-        };
-      })(options.stem, type);
-    } else {
-      key = (function(stem, type) {
-        var counter = 0;
-        const map = new WeakMap();
-        return inst => {
-          let isArray = Array.isArray(inst);
-          let thrw = ((type !== 'object' || isArray) &&
-            !(type === 'array' && isArray));
-          if (thrw) {
-            throw new TypeError(
-              `Expected type ${type} for ${JSON.stringify(inst)}`);
-          }
-          var key = map.get(inst);
-          if (!key) {
-            counter++;
-            key = stem + counter;
-            map.set(inst, key);
-          }
-          return key;
-        };
-      })(options.stem, type);
-    }
-  } else {
-    key = (function(stem) {
-      var counter = 0;
-      const map = new WeakMap();
-      return inst => {
-        switch (typeof inst) {
-          case 'number':
-          case 'string': return stem + signature(inst);
-        }
-        var key = map.get(inst);
-        if (!key) {
-          counter++;
-          key = stem + counter;
-          map.set(inst, key);
-        }
-        return key;
-      };
-    })(options.stem);
-  }
-
-  if (options.idProperty) {
-    return (function(idProperty, key) {
-      return inst => {
-        if (!inst[idProperty]) {
-          throw new ReferenceError(
-            `No property '${idProperty}' defined for ${JSON.stringify(inst)}`);
-        }
-        return key(inst[idProperty]);
-      };
-    })(options.idProperty, key);
-  }
-
-  return key;
-}
-
-function looseKeyFunc(options = {}) {
-  options = Object.assign({stem: 'key'}, options);
-
-  const key = (function(stem) {
-    return inst => stem + signature(inst);
-  })(options.stem);
-
-  if (options.idProperty) {
-    return (function(idProperty, key) {
-      return inst => {
-        if (!inst[idProperty]) {
-          throw new ReferenceError(
-            `No property '${idProperty}' defined for ${JSON.stringify(inst)}`);
-        }
-        return key(inst[idProperty]);
-      };
-    })(options.idProperty, key);
-  }
-
-  return key;
-}
+import isString from 'is-string';
+import objectFunc from './objectfunc';
 
 export default function keyFunc(...args) {
 
   const func = option => {
-    if (option === 'strict') {
-      return strictKeyFunc(option);
-    } else if (option === 'loose') {
-      return looseKeyFunc(option);
-    } else if (option.idProperty || option.stem) {
-      return option.loose ? looseKeyFunc(option) : strictKeyFunc(option);
+
+    if (isString(option)) {
+      option = {
+        type: option
+      };
     }
+
+    if (option.property) {
+      option.type = 'property';
+    }
+
+    return objectFunc(option.type, option);
+
   };
 
   return (function(keyFuncs) {
+    const max = keyFuncs.length - 1;
+
     return function(...args) {
+
       return args.map((arg, i) => {
-        return keyFuncs[i](arg);
+        return keyFuncs[i < max ? i : max](arg);
       }).join('_');
+
     };
+
   })(args.map(func));
+
 };
