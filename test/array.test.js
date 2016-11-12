555bc0102dff268c5e003e0a9053be3ab6c7aba6
Jason Lenoble <jason.lenoble@gmail.com>
2016-11-12 13:00:45 +0100

    arrayFunc key generator

---
+++ test/array.test.js
@@ -0,0 +1,49 @@
+import {expect} from 'chai';
+import signature from 'sig';
+import arrayFunc from '../src/arrayfunc';
+
+describe(`Testing arrayFunc`, function() {
+
+  it(`Calling arrayFunc()`,
+  function() {
+    const key = arrayFunc();
+
+    expect(key([console])).to.equal(key([console]));
+    expect(key([console, console])).to.equal(key([console, console]));
+
+    expect(() => key(console)).to.throw(TypeError,
+      'arr.map is not a function');
+
+    expect(key([console, console])).not.to.equal(key([console]));
+    expect(key([console])).not.to.equal(key([console, console]));
+
+    const obj = {id: 1};
+    expect(key([console, obj])).to.equal(key([console, obj]));
+    expect(key([console, obj])).not.to.equal(key([console, {id: 1}]));
+    expect(key([console, obj])).not.to.equal(key([obj, console]));
+
+    expect(key([console, obj])).to.equal(signature(['1', '2']));
+  });
+
+  it(`Calling arrayFunc('first')`,
+  function() {
+    const key = arrayFunc('key-');
+
+    expect(key([console])).to.equal(key([console]));
+    expect(key([console, console])).to.equal(key([console, console]));
+
+    expect(() => key(console)).to.throw(TypeError,
+      'arr.map is not a function');
+
+    expect(key([console, console])).not.to.equal(key([console]));
+    expect(key([console])).not.to.equal(key([console, console]));
+
+    const obj = {id: 1};
+    expect(key([console, obj])).to.equal(key([console, obj]));
+    expect(key([console, obj])).not.to.equal(key([console, {id: 1}]));
+    expect(key([console, obj])).not.to.equal(key([obj, console]));
+
+    expect(key([console, obj])).to.equal(signature(['key-1', 'key-2']));
+  });
+
+});
