3de4481e7151bd56c5aae937f186442957ee480f
Jason Lenoble <jason.lenoble@gmail.com>
2016-11-27 20:41:22 +0100

    Options array:* and set:*

--- src/objectfunc.js
+++ src/objectfunc.js
@@ -5,6 +5,34 @@ import propertyFunc from './propertyfunc';
 import arrayFunc from './arrayfunc';
 import setFunc from './setfunc';
 
+function whichFunc(type, prop) {
+
+  switch (type) {
+
+    case 'object':
+      return strictFunc;
+      break;
+
+    case 'literal':
+      return looseFunc;
+      break;
+
+    case 'property':
+      return propertyFunc.bind(undefined, prop);
+      break;
+
+    case 'array':
+      return arrayFunc;
+      break;
+
+    case 'set':
+      return setFunc;
+      break;
+
+  }
+
+}
+
 export default function objectFunc(type, stem) {
 
   if (!isString(type)) {
@@ -13,6 +41,18 @@ export default function objectFunc(type, stem) {
 
   if (!stem) {
     stem = {};
+  }
+
+  let match = type.match(/(\w+):(\w+)(:)?(\w+)?/);
+  let prop;
+  if (match) {
+    type = match[1];
+    if (match[3] === ':') {
+      prop = match[4];
+    }
+    match = match[2];
+  } else {
+    match = undefined;
   }
 
   switch (type) {
@@ -30,11 +70,11 @@ export default function objectFunc(type, stem) {
       break;
 
     case 'array':
-      return arrayFunc(stem.stem || stem);
+      return arrayFunc(stem.stem || stem, whichFunc(match, prop));
       break;
 
     case 'set':
-      return setFunc(stem.stem || stem);
+      return setFunc(stem.stem || stem, whichFunc(match, prop));
       break;
 
     default:
---
+++ test/advanced.test.js
@@ -0,0 +1,143 @@
+import {expect} from 'chai';
+import signature from 'sig';
+import keyFunc from '../src/keyfunc';
+
+describe('Testing keyFunc with option array:*', function() {
+
+  it(`Calling keyFunc('array:object')` , function() {
+    const key = keyFunc('array:object');
+
+    const obj = {id: 1};
+    expect(key([console, obj])).to.equal(key([console, obj]));
+    expect(key([console, obj])).not.to.equal(key([obj, console]));
+    expect(key([console, obj])).to.equal(signature(['1', '2']));
+  });
+
+  it(`Calling keyFunc('array:literal')` , function() {
+    const key = keyFunc('array:literal');
+
+    const obj = {id: 1};
+    expect(key([console, obj])).to.equal(key([console, obj]));
+    expect(key([console, obj])).not.to.equal(key([obj, console]));
+    expect(key([console, obj])).to.equal(signature([
+      signature(console), signature(obj)
+    ]));
+  });
+
+  it(`Calling keyFunc('array:property:id')` , function() {
+    const key = keyFunc('array:property:id');
+
+    const obj = {id: 1};
+    const obj2 = {id: 2};
+    expect(key([obj2, obj])).to.equal(key([obj2, obj]));
+    expect(key([obj2, obj])).not.to.equal(key([obj, obj2]));
+    expect(key([obj2, obj])).to.equal(signature([
+      signature(obj2.id), signature(obj.id)
+    ]));
+  });
+
+  it(`Calling keyFunc('array:array')` , function() {
+    const key = keyFunc('array:array');
+
+    const o1 = {};
+    const o2 = {id: 1};
+    const o3 = {name: 'Karl'};
+
+    const obj = [o1, o2, o3];
+    const obj2 = [o2, o3];
+    expect(key([obj2, obj])).to.equal(key([obj2, obj]));
+    expect(key([obj2, obj])).not.to.equal(key([obj, obj2]));
+    expect(key([obj2, obj])).to.equal(signature([
+      signature(['1', '2']),
+      signature(['3', '1', '2'])
+    ]));
+  });
+
+  it(`Calling keyFunc('array:set')` , function() {
+    const key = keyFunc('array:set');
+
+    const o1 = {};
+    const o2 = {id: 1};
+    const o3 = {name: 'Karl'};
+
+    const obj = [o1, o2, o3];
+    const obj2 = [o2, o3];
+    expect(key([obj2, obj])).to.equal(key([obj2, obj]));
+    expect(key([obj2, obj])).not.to.equal(key([obj, obj2]));
+    expect(key([obj2, obj])).to.equal(signature([
+      signature(['1', '2']),
+      signature(['1', '2', '3'])
+    ]));
+  });
+
+});
+
+describe('Testing keyFunc with option set:*', function() {
+
+  it(`Calling keyFunc('set:object')` , function() {
+    const key = keyFunc('set:object');
+
+    const obj = {id: 1};
+    expect(key([console, obj])).to.equal(key([console, obj]));
+    expect(key([console, obj])).to.equal(key([obj, console]));
+    expect(key([console, obj])).to.equal(signature(['1', '2']));
+  });
+
+  it(`Calling keyFunc('set:literal')` , function() {
+    const key = keyFunc('set:literal');
+
+    const obj = {id: 1};
+    expect(key([console, obj])).to.equal(key([console, obj]));
+    expect(key([console, obj])).to.equal(key([obj, console]));
+    expect(key([console, obj])).to.equal(signature([
+      signature(console), signature(obj)
+    ].sort()));
+  });
+
+  it(`Calling keyFunc('set:property:id')` , function() {
+    const key = keyFunc('set:property:id');
+
+    const obj = {id: 1};
+    const obj2 = {id: 2};
+    expect(key([obj2, obj])).to.equal(key([obj2, obj]));
+    expect(key([obj2, obj])).to.equal(key([obj, obj2]));
+    expect(key([obj2, obj])).to.equal(signature([
+      signature(obj2.id), signature(obj.id)
+    ].sort()));
+  });
+
+  it(`Calling keyFunc('set:array')` , function() {
+    const key = keyFunc('set:array');
+
+    const o1 = {};
+    const o2 = {id: 1};
+    const o3 = {name: 'Karl'};
+
+    const obj = [o1, o2, o3];
+    const obj2 = [o2, o3];
+    expect(key([obj2, obj])).to.equal(key([obj2, obj]));
+    expect(key([obj2, obj])).to.equal(key([obj, obj2]));
+    expect(key([obj2, obj])).to.equal(signature([
+      signature(['1', '2']),
+      signature(['3', '1', '2'])
+    ].sort()));
+  });
+
+  it(`Calling keyFunc('set:set')` , function() {
+    const key = keyFunc('set:set');
+
+    const o1 = {};
+    const o2 = {id: 1};
+    const o3 = {name: 'Karl'};
+
+    const obj = [o1, o2, o3];
+    const obj2 = [o2, o3];
+    expect(key([obj2, obj])).to.equal(key([obj2, obj]));
+    expect(key([obj2, obj])).to.equal(key([obj, obj2]));
+    expect(key([obj2, obj])).to.equal(signature([
+      signature(['1', '2']),
+      signature(['1', '2', '3'])
+    ].sort()));
+  });
+
+});
