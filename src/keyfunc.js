82d36153d6538f708fe69818717a1da5f974c07f
Jason Lenoble <jason.lenoble@gmail.com>
2016-11-30 17:05:12 +0100

    Simplified deep property case in keyfunc.js
    Now the heavy lifting is done by strictFunc directly

--- src/keyfunc.js
+++ src/keyfunc.js
@@ -1,5 +1,6 @@
 import isString from 'is-string';
 import objectFunc from './objectfunc';
+import propertyFunc from './propertyfunc';
 import signature from 'sig';
 
 export default function keyFunc(...args) {
@@ -24,19 +25,8 @@ export default function keyFunc(...args) {
           };
         }(option.stem ? option.stem : '', keyFunc(...option.sub)));
       } else if (option.type === 'property') {
-        return (function(stem, prop, key) {
-          return function(args) {
-            args = [args, ...prop.split(':')].reduce((obj, ppty) => {
-              if (!obj[ppty]) {
-                throw new ReferenceError(
-                  `Can't generate key for object with no property '${ppty}'`);
-              }
-              return obj[ppty];
-            });
-            return stem + key(args);
-          };
-        }(option.stem ? option.stem : '', option.property,
-          keyFunc(option.sub)));
+        return propertyFunc(option.property, option.stem ? option.stem : '',
+          keyFunc(option.sub));
       } // else ignore option sub
     }
 
