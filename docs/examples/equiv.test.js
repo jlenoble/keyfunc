2c18b547374241bba6ac13e9f4f26c6bbbc086c6
Jason Lenoble <jason.lenoble@gmail.com>
2017-11-28 00:36:35 +0100

    Complexified example to show power of function

--- docs/examples/equiv.test.js
+++ docs/examples/equiv.test.js
@@ -1,16 +1,36 @@
-import keyfunc, {equiv} from '../../src/keyfunc';
+import {equiv} from '../../src/keyfunc';
 import {expect} from 'chai';
 
 describe(`equiv factory example`, function () {
   it(``, function () {
     const eq = equiv({type: 'set', sub: {ntimes: 3}});
-    const key = keyfunc({type: 'set', sub: {ntimes: 3}});
 
     const obj1 = {id: 1};
     const obj2 = {id: 2};
     const obj3 = {id: 3};
+    const obj4 = {id: 3};
 
-    expect(eq([obj1, obj2, obj3], [obj3, obj2, obj1])).to.be.true;
-    expect(key([obj1, obj2, obj3])).to.equal(key([obj3, obj2, obj1]));
+    expect(eq(
+      [obj1, obj2, obj3],
+      [obj1, obj3, obj2],
+      [obj2, obj1, obj3],
+      [obj2, obj3, obj1],
+      [obj3, obj1, obj2],
+      [obj3, obj2, obj1]
+    )).to.be.true;
+
+    expect(eq(
+      [obj1, obj2, obj3],
+      [obj1, obj2, obj4]
+    )).to.be.false;
+
+    expect(eq(
+      [obj1, obj2, obj3],
+      [obj1, obj3, obj2],
+      [obj2, obj1, obj3],
+      [obj2, obj4, obj1], // mismatch here
+      [obj3, obj1, obj2],
+      [obj3, obj2, obj1]
+    )).to.be.false;
   });
 });
